1eae5cf353b231086f9a25d391236762b4be2c34
chesterchenn <chesterchenn@outlook.com>
2022-02-17 11:01:36 +0800

    build: chalk 版本回降4.1.2

--- bin/start.ts
+++ bin/start.ts
@@ -1,10 +1,9 @@
-
 /**
  * 校验服务启动前是否进行初始化
  */
-const spawn = require('cross-spawn');
-const chalk = require('chalk');
-const { isJavaScript, isTypeScript } = require('../paths.ts');
+import spawn from 'cross-spawn';
+import chalk from 'chalk';
+import { isJavaScript, isTypeScript } from '../paths';
 
 const exec = () => {
   const args = ['serve', '--config', 'webpack/webpack.dev.ts'];
@@ -12,8 +11,13 @@ const exec = () => {
 };
 
 if (!isTypeScript && !isJavaScript) {
-  console.error(chalk.red('You need to run ') + chalk.green('npm run init ') + chalk.red('or ') 
-    + chalk.green('yarn run init:js ') + chalk.red('at first.'));
+  console.error(
+    chalk.red('You need to run ') +
+      chalk.green('npm run init ') +
+      chalk.red('or ') +
+      chalk.green('yarn run init:js ') +
+      chalk.red('at first.')
+  );
 } else {
   exec();
 }
