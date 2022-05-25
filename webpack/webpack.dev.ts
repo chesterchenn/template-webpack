c69a9904cf915eab552986c127df7d7d5f557051
chesterchenn <chesterchenn@outlook.com>
2022-05-25 17:41:31 +0800

    build: 删除弃用的webpack引入

--- webpack/webpack.dev.ts
+++ webpack/webpack.dev.ts
@@ -1,18 +1,18 @@
-import { merge } from "webpack-merge";
-import common from "./webpack.common";
-import { dist } from "../paths";
-import webpack, { Configuration } from "webpack";
+import { merge } from 'webpack-merge';
+import common from './webpack.common';
+import { dist } from '../paths';
+import { Configuration } from 'webpack';
 import 'webpack-dev-server';
 
 const config: Configuration = merge(common, {
-  mode: "development",
+  mode: 'development',
   devServer: {
     static: dist,
     port: 8080,
     open: true,
     hot: true,
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
 });
 
 export default config;
