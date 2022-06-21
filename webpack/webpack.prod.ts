55a20526177d439ac513e0d026bd2ac1ec5c3bd2
chesterchenn <chesterchenn@outlook.com>
2022-06-21 10:45:19 +0800

    ci: 构建成功自动退出进程

--- webpack/webpack.prod.ts
+++ webpack/webpack.prod.ts
@@ -1,9 +1,21 @@
-import { merge } from "webpack-merge";
-import common from "./webpack.common";
-import { Configuration } from "webpack";
+import { merge } from 'webpack-merge';
+import common from './webpack.common';
+import { Configuration, Compiler } from 'webpack';
 
 const config: Configuration = merge(common, {
-  mode: "production",
+  mode: 'production',
+  plugins: [
+    {
+      apply: (compiler: Compiler) => {
+        compiler.hooks.done.tap('done', () => {
+          console.log('Compile is done');
+          setTimeout(() => {
+            process.exit(0);
+          });
+        });
+      },
+    },
+  ],
 });
 
 export default config;
